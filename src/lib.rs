ae1eda7d263deb3b8e1ee093acf19ec0ebd21cf6
SunkenPotato <3.14radius02@gmail.com>
2024-11-16 20:23:05 +0100

    Finish K-Means implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,160 @@
+use std::collections::HashMap;
 
+use glam::Vec2;
+use index_vec::{index_vec, IndexVec};
+use rand::{seq::SliceRandom, thread_rng, Rng};
+
+const DEFAULT_K: usize = 4;
+const DEFAULT_N_SAMP: usize = 100;
+const DEFAULT_STD: f32 = 0.8;
+
+pub type Point = Vec2;
+
+index_vec::define_index_type! {
+    pub struct PointIdx = usize;
+}
+
+index_vec::define_index_type! {
+    pub struct CentroidIdx = usize;
+}
+
+struct CentroidDistance {
+    idx: CentroidIdx,
+    distance: f32,
+}
+
+fn random_point() -> Point {
+    let mut rng = thread_rng();
+    let (x, y) = (rng.gen_range(0_f32..20_f32), rng.gen_range(0_f32..20_f32));
+
+    Point::new(x, y)
+}
+
+pub fn generate_datapoints(
+    clusters: Option<usize>,
+    n_samp: Option<usize>,
+    std: Option<f32>,
+) -> IndexVec<PointIdx, Point> {
+    let clusters = match clusters {
+        Some(v) => v,
+        None => DEFAULT_K,
+    };
+
+    let n_samp = match n_samp {
+        Some(v) => v,
+        None => DEFAULT_N_SAMP,
+    };
+
+    let std = match std {
+        Some(v) => v,
+        None => DEFAULT_STD * 2.,
+    };
+
+    let mut centroids: IndexVec<PointIdx, Point> = index_vec![Point::default(); clusters];
+
+    for el in &mut centroids {
+        *el = random_point();
+    }
+
+    let mut points = index_vec!();
+
+    for ctr in centroids.iter() {
+        for _ in 0..n_samp / clusters {
+            let mut point = random_point();
+
+            while point.distance(*ctr) > std {
+                point = random_point();
+            }
+
+            points.push(point);
+        }
+    }
+
+    points.append(&mut centroids);
+
+    points
+}
+
+pub fn pick_centroids(
+    dataset: &IndexVec<PointIdx, Point>,
+    k: Option<usize>,
+) -> IndexVec<CentroidIdx, Point> {
+    let k = match k {
+        Some(v) => v,
+        None => DEFAULT_K,
+    };
+
+    let mut thread_rng = thread_rng();
+
+    let mut centroid_vec = index_vec![];
+
+    for _ in 0..k {
+        let random_centroid = dataset.raw.choose(&mut thread_rng).expect("non empty list");
+        centroid_vec.push(random_centroid.clone());
+    }
+
+    centroid_vec
+}
+
+pub fn associate_centroids_to_points<'d, 'c>(
+    dataset: &'d IndexVec<PointIdx, Point>,
+    centroids: &'c IndexVec<CentroidIdx, Point>,
+) -> HashMap<CentroidIdx, Vec<&'d Point>> {
+    let mut assoc: HashMap<CentroidIdx, Vec<&'d Point>> = HashMap::new();
+
+    for point in dataset {
+        let mut distances: Vec<CentroidDistance> = vec![];
+
+        for (idx, ctr) in centroids.iter_enumerated() {
+            let cdist = CentroidDistance {
+                idx,
+                distance: point.distance(*ctr),
+            };
+
+            distances.push(cdist);
+        }
+
+        let smallest = distances
+            .iter()
+            .min_by(|x, y| x.distance.partial_cmp(&y.distance).unwrap())
+            .expect("non-empty vec");
+
+        assoc.entry(smallest.idx).or_insert(vec![]).push(point);
+    }
+
+    assoc
+}
+
+pub fn update_centroids(assoc: &HashMap<CentroidIdx, Vec<&Point>>) -> IndexVec<CentroidIdx, Point> {
+    let mut new_centroids: IndexVec<CentroidIdx, Point> = index_vec!();
+
+    let centroid_indices = assoc.keys();
+
+    for index in centroid_indices {
+        let values = assoc.get(index).expect("valid indices returned by .keys()");
+        new_centroids.push(calculate_average_point(values));
+    }
+
+    new_centroids
+}
+
+fn calculate_average_point(points: &Vec<&Point>) -> Point {
+    let mut x: f32 = 0f32;
+    let mut y: f32 = 0f32;
+
+    let l = points.iter().len() as f32;
+
+    for point in points {
+        x += point.x;
+        y += point.y;
+    }
+
+    x /= l;
+    y /= l;
+
+    Point::new(x, y)
+}
+
+pub fn sort_point_vec(v: &mut IndexVec<CentroidIdx, Vec2>) {
+    v.sort_by(|a, b| a.x.total_cmp(&b.x).cmp(&a.y.total_cmp(&b.y)))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,22 @@
-use std::collections::HashMap;
-
-use glam::Vec2;
-use ordered_float::OrderedFloat;
-use rand::{seq::SliceRandom, thread_rng, Rng};
-
-type Point = Vec2;
-type Centroids<'a> = Vec<&'a Point>;
-type PCAssoc = HashMap<Point, Vec<Point>>;
-
-struct Point {
-    vec: Vec2,
-    id: usize,
-}
-
-struct CentroidDistance<'p> {
-    centroid: &'p Point,
-    distance: f32,
-}
-
-impl<'p> PartialEq for CentroidDistance<'p> {
-    fn eq(&self, other: &Self) -> bool {
-        self.centroid == other.centroid && self.distance == other.distance
-    }
-}
-
-impl<'p> PartialOrd for CentroidDistance<'p> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.distance.total_cmp(&other.distance))
-    }
-}
-
-impl<'p> CentroidDistance<'p> {
-    fn new(centroid: &'p Point, distance: f32) -> Self {
-        Self { centroid, distance }
-    }
-}
-
-const GRID_SIZE: (usize, usize) = (20, 20);
-
-fn init_centroids<'a, 'v>(d: &'v Vec<Point>, k: usize) -> Centroids<'a>
-where
-    'v: 'a,
-{
-    let mut centroids: Centroids = vec![];
-    for _ in 0..k {
-        // ok lol
-        let Some(centroid) = d.choose(&mut thread_rng()) else {
-            panic!("The dataset is empty!")
-        };
-        centroids.push(centroid);
-    }
-
-    centroids
-}
-
-fn generate_datapoints(
-    n_points: Option<usize>,
-    k_clusters: Option<usize>,
-    distribution: Option<f32>,
-) -> Vec<Point> {
-    let n_points = match n_points {
-        Some(v) => v,
-        None => 100,
-    };
-
-    let k_clusters = match k_clusters {
-        Some(v) => v,
-        None => 4,
-    };
-
-    let distribution = match distribution {
-        Some(v) => v,
-        None => 4.,
-    };
-
-    let mut centroids: Vec<Vec2> = vec![Vec2::default(); k_clusters];
-    centroids.fill_with(|| random_point());
-
-    let mut dataset: Vec<Point> = vec![];
-
-    for _centroid in centroids {
-        for _ in 0..(n_points / k_clusters) {
-            dataset.push(generate_point(_centroid, distribution))
-        }
-    }
-
-    dataset
-}
-
-fn generate_point(center: Point, rad: f32) -> Point {
-    let mut point: Point = Point::default();
-
-    while point.distance(center) > rad {
-        point = random_point();
-    }
-
-    point
-}
-
-fn random_point() -> Point {
-    let new_x: f32 = rand::thread_rng().gen_range(0_f32..GRID_SIZE.0 as f32);
-    let new_y: f32 = rand::thread_rng().gen_range(0_f32..GRID_SIZE.1 as f32);
-
-    Point::from_array([new_x, new_y])
-}
-
-fn random_centroids<'a, 'v>(dataset: &'v Vec<Point>, k: usize) -> Centroids<'a>
-where
-    'v: 'a,
-{
-    let mut centroids: Centroids = vec![];
-    for _ in 0..k {
-        let Some(random_point) = dataset.choose(&mut thread_rng()) else {
-            panic!("Dataset is empty!");
-        };
-        centroids.push(random_point);
-    }
-
-    centroids
-}
-
-fn assign_points_to_centroids(d: Vec<Vec2>, centroids: Centroids) -> PCAssoc {
-    let mut assocs: PCAssoc = HashMap::new();
-
-    for point in d {
-        let mut centroid_distances: Vec<CentroidDistance> = vec![];
-
-        for ctr in &centroids {
-            centroid_distances.push(CentroidDistance::new(*ctr, ctr.distance(point)))
-        }
-
-        assocs.insert(centroid_distances.iter().reduce(f32::MIN).unwrap())
-    }
-
-    assocs
-}
+use k_mea_rs::{
+    associate_centroids_to_points, generate_datapoints, pick_centroids, sort_point_vec,
+    update_centroids,
+};
 
 fn main() {
-    let k = 4; // Change!
+    let dataset = generate_datapoints(None, None, None);
+    let mut old_centroids = pick_centroids(&dataset, None);
 
-    let dataset = generate_datapoints(None, None, None); // Initialize dataset
-    let guessed_centroids = random_centroids(&dataset, k);
-
-    let mut old_centroids = guessed_centroids;
-
+    let mut ctr = 1u8;
     loop {
-        let associations = todo!();
+        ctr += 1;
+        let assoc = associate_centroids_to_points(&dataset, &old_centroids);
+        let mut new_centroids = update_centroids(&assoc);
+
+        if sort_point_vec(&mut new_centroids) == sort_point_vec(&mut old_centroids) {
+            break;
+        }
     }
+
+    println!("K-Means completed with n={ctr} runs");
 }
